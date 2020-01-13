7c387c7b18d5ac57f3b05fa3088e9c3bb92e75c6
snackk <snackk026@gmail.com>
2020-01-13 18:00:34 +0000

    some lost commits

--- src/app/api/status-cake-uptime-report.service.ts
+++ src/app/api/status-cake-uptime-report.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { StatusCakeReport } from '../model/statusCakeReport-model';
 
@@ -12,6 +12,15 @@ export class StatusCakeUptimeReportService {
   }
 
   public getReports(testId: number): Observable<StatusCakeReport[]> {
-    return this.http.get<StatusCakeReport[]>('./assets/api/status-cake-uptime-report.json');
+    /*
+    * curl -H "API:3VCi0OJreHfTC0y6pwkj" -H "Username: santosmatrixsapopt" -X GET https://app.statuscake.com/API/Tests
+    * curl -H "API:3VCi0OJreHfTC0y6pwkj" -H "Username: santosmatrixsapopt" -X GET https://app.statuscake.com/API/Tests/Periods/\?TestID\=5104031
+    * curl -H "API:3VCi0OJreHfTC0y6pwkj" -H "Username: santosmatrixsapopt" -X GET https://app.statuscake.com/API/Tests/Periods/\?TestID\=5202774
+    * */
+    const headers = new HttpHeaders({
+      'API': '3VCi0OJreHfTC0y6pwkj',
+      'Username': 'santosmatrixsapopt'
+    });
+    return this.http.get<StatusCakeReport[]>('/api/\?TestID\=1', headers);
   }
 }
