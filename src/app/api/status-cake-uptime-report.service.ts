ad6d7521897c080336d691c07db30fb53a612520
snackk <snackk026@gmail.com>
2020-01-14 14:39:44 +0000

    still no go

--- src/app/api/status-cake-uptime-report.service.ts
+++ src/app/api/status-cake-uptime-report.service.ts
@@ -1,7 +1,7 @@
-import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { Observable } from 'rxjs';
-import { StatusCakeReport } from '../model/statusCakeReport-model';
+import {Injectable} from '@angular/core';
+import {HttpClient, HttpHeaders, HttpParams} from '@angular/common/http';
+import {Observable} from 'rxjs';
+import {StatusCakeReport} from '../model/statusCakeReport-model';
 
 @Injectable({
   providedIn: 'root'
@@ -17,10 +17,14 @@ export class StatusCakeUptimeReportService {
     * curl -H "API:3VCi0OJreHfTC0y6pwkj" -H "Username: santosmatrixsapopt" -X GET https://app.statuscake.com/API/Tests/Periods/\?TestID\=5104031
     * curl -H "API:3VCi0OJreHfTC0y6pwkj" -H "Username: santosmatrixsapopt" -X GET https://app.statuscake.com/API/Tests/Periods/\?TestID\=5202774
     * */
-    const headers = new HttpHeaders({
-      'API': '3VCi0OJreHfTC0y6pwkj',
-      'Username': 'santosmatrixsapopt'
-    });
-    return this.http.get<StatusCakeReport[]>('/api/\?TestID\=1', headers);
+
+    const headers = new HttpHeaders()
+    .set('API', '3VCi0OJreHfTC0y6pwkj')
+    .set('Username', 'santosmatrixsapopt');
+
+    const myObject: any = { TestID: '5104031'};
+
+    const options = { params: new HttpParams({ fromObject: myObject }), headers: headers };
+    return this.http.get<StatusCakeReport[]>('/api/', options);
   }
 }
